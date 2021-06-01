41e491c5fcb30ef0e2544e433cf7c6c63e3eda5d
scr26 <32083670+scr26@users.noreply.github.com>
2021-06-01 07:47:48 -1000

    insert db and render templates

--- backend.py
+++ backend.py
@@ -1,42 +1,47 @@
-
-import time
-import flask 
+from Flask import flask, request, render_template, redirect
 import os
-import random
-import requests
-import urllib.request
-import urllib
-import wget
+import sqlite3
 
+currentlocation = os.path.dirname(os.path.abspath(__file))
 
+myapp = Flask(__name__)
 
-app = flask.Flask(__name__)
- 
-@app.route('/')
- 
-def index():
+@myapp.route("/")
 
-    hello = "Hello World"
-    
-    
-    
-    
+def homepage():
+    return render_templae("homepage.html")
 
-    
-    return flask.render_template(
-        
-        "index.html",
-        hello=hello
-        
-        
-        
-        )
-    
-app.run(
-    
-    port=int(os.getenv('PORT',8080)),
-    host = os.getenv('IP','0.0.0.0')
-    
-    )
-    
-    
+@myapp.route("/",methods = ["POST"])
+def checklogin():
+    UN = request.form['Username']
+    PW = request.form['Password']
+
+    sqlconnection = sqlite3.Connection(currentlocation + "\Login.db")
+    cursor = sqlconnection.cursor()
+    query1 = "SELECT Username, Password From Users WHERE Username = {un} AND Password = {pw}".format(un = UN, pw = PW)
+
+    rows = cursor.execute(query1)
+    rows = rows.fetchall()
+
+    if len(rows) == 1:
+        return render_template("LogginIn.html")
+    else:
+        return redirect("/register")
+
+@myapp.route("/register",methods= ["GET","POST"])
+
+def registerpage():
+    if request.method == "POST":
+        dUN = request.form['Username']
+        dPW = request.form['Dpassword']
+        Uemail = request.form['EmalUser']
+        sqlconnection = sqlite3.Connection(currentlocation + "\Login.db")
+        cursor = sqlconnection.cursor()
+        query1 = "INSERT INTO Users Values('{u}','{p}','{e}')".format(u = dUN,p = dPW, e = Uemail )
+        cursor.execute(query1)
+        sqlconnection.commit()
+        return redirect("/")
+    return render_template("Register.html")
+
+if __name__ == '__main__':
+    myapp.run()
